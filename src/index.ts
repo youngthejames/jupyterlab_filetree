dd2213f738f0ad417a35127cab8e1f9f7bbc9275
James Young <jtyoung1292@gmail.com>
2018-10-03 11:33:33 -0400

    grid and collapse working

--- src/index.ts
+++ src/index.ts
@@ -28,45 +28,44 @@ function switchView(mode: any) {
 function toggleFolder(row: any, parent: any) {
   let children = parent.children;
   for(let i = 0; i < children.length; i++) {
-	if(children[i] != row) {
+	if(children[i].id.startsWith(row.id) && children[i] != row) {
 	  children[i].style.display = switchView(children[i].style.display);
 	}
   }
 }
 
-function buildTableContents(body: any, data: any, level: number) {
+function buildTableContents(body: any, data: any, level: number, parent: string) {
   Object.keys(data).forEach(key => {
     let tr = document.createElement('tr');
     let td = document.createElement('td');
 
     let icon = document.createElement('span');
     icon.className = 'jp-DirListing-itemIcon jp-MaterialIcon ';
-    if(typeof data[key] !== 'string')
+    if(!(data[key] instanceof Array))
       icon.className += 'jp-OpenFolderIcon';
     else
       icon.className += 'jp-FileIcon';
     
     td.appendChild(icon);  
-    let title = document.createElement('span')
+    let title = document.createElement('span');
     title.innerHTML = key;
-    td.appendChild(title);    
+    td.appendChild(title);
     td.className = 'filetree-item-text'; 
     td.style.setProperty('--indent', level + 'em');
 
     tr.appendChild(td);
     tr.className = 'filetree-item';
+    tr.id = parent + '-' + key
 
     if (!(data[key] instanceof Array)) {
-      var tbody = document.createElement('tbody');
-      tbody.appendChild(tr);
-      tr.onclick = function() { toggleFolder(tr, tbody); }
-      buildTableContents(tbody, data[key], level+1);
-      body.appendChild(tbody);
+      tr.onclick = function() { toggleFolder(tr, body); }
+      body.appendChild(tr);
+      buildTableContents(body, data[key], level+1, tr.id);
     } else {
-      tr.onclick = function() {}
+      tr.onclick = function() {console.log('open file');}
       for (var value in data[key]) {
         var temp = document.createElement('td');
-        temp.innerHTML = value;
+        temp.innerHTML = data[key][value];
         tr.appendChild(temp);
       }
       body.appendChild(tr);
@@ -88,7 +87,8 @@ function buildTable(headers: any, data: any) {
   thead.appendChild(headRow);
   table.appendChild(thead);
 
-  buildTableContents(tbody, data, 1);
+  buildTableContents(tbody, data, 1, '');
+
   table.appendChild(tbody);
 
   return table;
@@ -99,10 +99,6 @@ function activate(app: JupyterLab, restorer: ILayoutRestorer) {
   
   let widget = new FileTreeWidget();
   restorer.add(widget, 'filetree-jupyterlab');
-
-  let header = document.createElement('header');
-  header.textContent = 'File Tree';
-  widget.node.appendChild(header);
 
   function callback(resp: any) {
       var table = buildTable(resp['meta'], resp['files']);
